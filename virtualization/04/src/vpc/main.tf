03d1462d029288d08c69484c848bd71f120244fa
gambrilus <gambrilusrus@gmail.com>
2023-07-26 13:35:17 +0000

    terraform-04-update

--- virtualization/04/src/vpc/main.tf
+++ virtualization/04/src/vpc/main.tf
@@ -15,7 +15,7 @@ resource "yandex_vpc_network" "develop" {
 #создаем подсеть
 resource "yandex_vpc_subnet" "develop" {
   count          = length(var.subnets)
-  name           = var.env_name
+  name           = "${var.env_name}-${count.index}"
   zone           = var.subnets[count.index].zone
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.subnets[count.index].cidr
